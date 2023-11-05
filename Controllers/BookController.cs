371d83a44e28576b630837e322222ed34e977951
Per(k) <aupper094@gmail.com>
2023-11-06 00:32:13 +0700

    Add Delete Book

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -11,6 +11,7 @@ namespace BasicWebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+
     public class BookController : ControllerBase
     {
         [HttpGet]
@@ -175,6 +176,234 @@ namespace BasicWebApi.Controllers
                 else
                 {
                     return StatusCode(StatusCodes.Status501NotImplemented, new { message = "delete erro" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [Authorize]
+        public IActionResult Sale(string barcode)
+        {
+            try
+            {
+                // 1 find product
+                int id = 0;
+                int price = 0;
+                {
+                    using NpgsqlConnection conn = new Connect().GetConnection();
+                    using NpgsqlCommand cmd = conn.CreateCommand();
+
+                    cmd.CommandText = "SELECT id, price FROM tb_book WHERE isbn = @barcode";
+                    cmd.Parameters.AddWithValue("barcode", barcode);
+
+                    using NpgsqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        id = Convert.ToInt32(reader["id"]);
+                        price = Convert.ToInt32(reader["price"]);
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status501NotImplemented, new { message = "not found barcode" });
+                    }
+                }
+
+                // 2 create bill sale
+                int billSaleId = 0;
+
+                {
+                    using NpgsqlConnection conn = new Connect().GetConnection();
+                    using NpgsqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = "SELECT id FROM tb_bill_sale WHERE pay_at IS NULL";
+
+                    using NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        billSaleId = Convert.ToInt32(reader["id"]);
+                    }
+                }
+
+                if (billSaleId == 0)
+                {
+                    {
+                        using NpgsqlConnection conn = new Connect().GetConnection();
+                        using NpgsqlCommand cmd = conn.CreateCommand();
+                        cmd.CommandText = "INSERT INTO tb_bill_sale(created_at) VALUES(NOW()) RETURNING id";
+                        int result = cmd.ExecuteNonQuery();
+                        //using NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                        return Ok(new { id = result });
+                    }
+                }
+
+
+                // 3) create bill sale detail
+                {
+                    bool foundProduct = false;
+
+                    {
+                        using NpgsqlConnection conn = new Connect().GetConnection();
+                        using NpgsqlCommand cmd = conn.CreateCommand();
+                        cmd.CommandText = @"
+                            SELECT COUNT(id) AS totalRow FROM tb_bill_sale_detail
+                            WHERE bill_sale_id = @bill_sale_id AND book_id = @book_id
+                        ";
+                        cmd.Parameters.AddWithValue("bill_sale_id", billSaleId);
+                        cmd.Parameters.AddWithValue("book_id", id);
+
+                        using NpgsqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            int totalRow = Convert.ToInt32(reader["totalRow"]);
+                            foundProduct = totalRow > 0;
+
+                        }
+                    }
+
+                    if (foundProduct)
+                    {
+                        //update
+                        using NpgsqlConnection conn = new Connect().GetConnection();
+                        using NpgsqlCommand cmd = conn.CreateCommand();
+                        cmd.CommandText = @"
+                            UPDATE tb_bill_sale_detail 
+                            SET qty = qty + 1
+                            WHERE bill_sale_id = @bill_sale_id AND book_id = @book_id
+                        ";
+                        cmd.Parameters.AddWithValue("bill_sale_id", billSaleId);
+                        cmd.Parameters.AddWithValue("book_id", id);
+
+                        if (cmd.ExecuteNonQuery() != -1)
+                        {
+                            return Ok(new { message = "success", billSaleId = billSaleId });
+                        }
+                    }
+                    else
+                    {
+                        //insert
+                        using NpgsqlConnection conn = new Connect().GetConnection();
+                        using NpgsqlCommand cmd = conn.CreateCommand();
+                        cmd.CommandText = @"
+                            INSERT INTO tb_bill_sale_detail(bill_sale_id, book_id, price, qty)
+                            VALUES(@bill_sale_id, @book_id, @price, @1)
+                        ";
+                        cmd.Parameters.AddWithValue("bill_sale_id", billSaleId);
+                        cmd.Parameters.AddWithValue("book_id", id);
+                        cmd.Parameters.AddWithValue("price", price);
+
+                        if (cmd.ExecuteNonQuery() != -1)
+                        {
+                            return Ok(new { message = "success", billSaleId = billSaleId });
+                        }
+                    }
+
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("[action]/{billSaleId}")]
+        [Authorize]
+        public IActionResult BillSaleInfo(int billSaleId)
+        {
+            try
+            {
+                using NpgsqlConnection connect = new Connect().GetConnection();
+                using NpgsqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = @"
+                    SELECT
+                        tb_bill_sale_detail.id,
+                        tb_bill_sale_detail.qty,
+                        tb_bill_sale_detail.price,
+                        tb_book.name,
+                        tb_book.isbn
+                    FROM tb_bill_sale_detail 
+                    LEFT JOIN tb_book ON tb_book.id = tb_bill_sale_detail.book_id 
+                    WHERE bill_sale_id = @bill_sale_id
+                    ORDER BY tb_bill_sale_detail.id DESC
+                ";
+                cmd.Parameters.AddWithValue("bill_sale_id", billSaleId);
+
+                using NpgsqlDataReader reader = cmd.ExecuteReader();
+                List<object> list = new List<object>();
+
+                while (reader.Read())
+                {
+                    list.Add(new
+                    {
+                        price = Convert.ToInt32(reader["price"]),
+                        qty = Convert.ToInt32(reader["qty"]),
+                        id = Convert.ToInt32(reader["id"]),
+                        isbn = reader["isbn"].ToString(),
+                        name = reader["name"].ToString()
+                    });
+                }
+
+                return Ok(new { results = list });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [Authorize]
+        public IActionResult RecentBillSale()
+        {
+            try
+            {
+                using NpgsqlConnection connect = new Connect().GetConnection();
+                using NpgsqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = "SELECT id FROM tb_bill_sale WHERE pay_at IS NULL";
+
+                using NpgsqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return Ok(new
+                    {
+                        billSaleId = Convert.ToInt32(reader["id"])
+                    });
+                }
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        [Route("[action]/{id}")]
+        [Authorize]
+        public IActionResult DeleteSaleItem(int id)
+        {
+            try
+            {
+                using NpgsqlConnection connect = new Connect().GetConnection();
+                using NpgsqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = "DELETE FROM tb_bill_sale_detail WHERE id = @id";
+                cmd.Parameters.AddWithValue("id", id);
+
+                if (cmd.ExecuteNonQuery() != -1)
+                {
+                    return Ok(new { message = "success" });
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status501NotImplemented, new { message = "delete error" });
                 }
             }
             catch (Exception ex)
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +21,35 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+}
+
+);
 
 // Authorization and Authentication
 builder.Services.AddAuthentication(options =>
